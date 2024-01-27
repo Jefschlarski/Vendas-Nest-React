a4cb1b0cf065479aaa0cf446e6794ab21032d382
Jeferson <jefersonunik@gmai.com>
2024-01-26 23:31:03 -0300

    criando entidades e integrando com postgres

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,21 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserDto } from './dto/user.dto';
+import { UserService } from './user.service';
+import { User } from './models/user.entity';
 
 @Controller('user')
 export class UserController {
 
+    constructor(private readonly userService: UserService){}
+
     @Post()
     async create (@Body() user: UserDto)
     {
-        return user;
+        return this.userService.create(user);
     }
 
+    @Get()
+    async getAll(): Promise<User[]> {
+        return this.userService.getAll();
+    }
 }
---
+++ src/user/user.service.ts
@@ -0,0 +1,30 @@
+import { UserDto } from './dto/user.dto';
+import { User } from './models/user.entity';
+import { hash } from 'bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserService {
+   constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+   ){};
+
+    async create(userDto: UserDto)
+    {
+        const saltOrRounds = 10;
+        const passwordHashed = await hash(userDto.password, saltOrRounds);  
+
+        return this.userRepository.save(
+            {
+                ...userDto,
+                password: passwordHashed
+            })
+    }
+
+    async getAll(): Promise<User[]>{
+        return this.userRepository.find();
+    }
+}
